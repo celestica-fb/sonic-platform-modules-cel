d27531e2de54aa82f38ecac571443eaa3381c74f
Wirut Getbamrung <wgetbum@icloud.com>
2019-05-27 17:00:01 +0700

    [platform/cel]: Update BMC watchdog polling service solution

--- tools/bmc_wdt/bmc_wdt.sh
+++ tools/bmc_wdt/bmc_wdt.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 #
-# watchdog     Script to control bmc watchdog 
+# watchdog     Script to control bmc watchdog
 
-# start bmcwdt 
+# start bmcwdt
 start(){
-        curl -m 10 -d '{"wdt":"60"}' http://240.1.1.1:8080/api/sys/watchdog
+       curl -m 10 -d '{"wdt":"60"}' http://240.1.1.1:8080/api/sys/watchdog >/dev/null 2>&1
 }
 
 # stop bmcwdt
 stop(){
-        curl -m 10 -d '{"wdt":"0"}' http://240.1.1.1:8080/api/sys/watchdog
+       curl -m 10 -d '{"wdt":"0"}' http://240.1.1.1:8080/api/sys/watchdog >/dev/null 2>&1
 }
 
 case "$1" in
-        start)
-                start 
-                ;;
-        stop)
-                stop 
-                ;;
-        *)
-        echo $"Usage: $0 {start|stop}" 
+       start)
+               start
+               ;;
+       stop)
+               stop
+               ;;
+       *)
+       echo $"Usage: $0 {start|stop}"
 esac
