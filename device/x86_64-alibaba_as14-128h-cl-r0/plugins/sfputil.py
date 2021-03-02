d6dd91673339a2a01fa51970d29b3ed7b5afb9fb
Maxwill Ma <mamax@celestica.com>
2021-03-02 11:20:46 +0800

    move QSFP56 eeprom content validation into pluins/sfputil.py
    Signed-off-by: Maxwill Ma <mamax@celestica.com>

--- device/x86_64-alibaba_as14-128h-cl-r0/plugins/sfputil.py
+++ device/x86_64-alibaba_as14-128h-cl-r0/plugins/sfputil.py
@@ -5,6 +5,7 @@
 
 try:
     import time
+    import json
     from sonic_sfp.sfputilbase import SfpUtilBase
 except ImportError as e:
     raise ImportError("%s - required module not found" % str(e))
@@ -245,3 +246,168 @@ class SfpUtil(SfpUtilBase):
             self._do_write_file(reg_file, 0, 1)
 
         return result
+
+# +++ add for optical module eeprom content validation +++ #
+    # load reference stand file
+    def load_stand_sfp_table(self):
+        try:
+            with open(self.stand_json_path) as stand_sfp_jason:
+                self.stand_sfp_table = json.load(stand_sfp_jason)
+                return self.stand_sfp_table
+        except IOError:
+            return None
+
+    def _generate_stand_port_sfp_table(self):
+        self.stand_sfp_table = [0] * len(self.qsfp_ports)
+        for port_num in self.qsfp_ports:
+            if self.get_presence(port_num):
+                print("port_num==============%s" % port_num)
+                self.stand_sfp_table[port_num - 1] = self.get_eeprom_dict(port_num)
+                print("Successfully generated dict of eeprom.")
+                stand_sfp_thr = self.get_transceiver_dom_threshold_info_dict(port_num)
+                self.stand_sfp_table[port_num - 1]['threshold'] = stand_sfp_thr
+                print("Successfully get threshold data.")
+                print("Done! Successfully dumped E2PROM information of port %s" % port_num)
+        try:
+            if not os.path.exists(self.stand_json_path):
+                with open(self.stand_json_path, "w") as f:
+                    json.dump(self.stand_sfp_table, f, indent=2)
+        except IOError:
+            return False
+
+
+    def generate_stand_sfp_table(self):
+        self.stand_sfp_table = self.load_stand_sfp_table()
+        if self.stand_sfp_table:
+            return None
+
+        if self.osfp_ports:
+            self._generate_stand_port_sfp_table()
+        if self.qsfp_ports:
+            self._generate_stand_port_sfp_table()
+
+    def check_sfp_fixed(self, cur_sfp_fix_info, std_sfp_fix_info):
+        log_info = ""
+        if std_sfp_fix_info == cur_sfp_fix_info:
+            return log_info
+        elif std_sfp_fix_info.keys() == cur_sfp_fix_info.keys():
+            for key in std_sfp_fix_info.keys():
+                if std_sfp_fix_info[key] != cur_sfp_fix_info[key]:
+                    log_info += "\tError  : error in key: %s, expect value: %s  current value:%s\n"\
+                                %(key, std_sfp_fix_info[key],cur_sfp_fix_info[key])
+        elif std_sfp_fix_info.keys() in cur_sfp_fix_info.keys():
+            log_info += "\tError  : Missing fix key : %s in current sfp dump\n"\
+                        %(std_sfp_fix_info.keys() - cur_sfp_fix_info.keys())
+        elif cur_sfp_fix_info.keys() in std_sfp_fix_info.keys():
+            log_info += "\tError  : Additional fix key : %s in current sfp dump\n"\
+                        %(cur_sfp_fix_info.keys() - std_sfp_fix_info.keys())
+        else:
+            log_info += "\tError  : Unknow error in fix date check\n"
+            # pdb.set_trace()
+        return log_info
+
+    def check_sfp_var_with_thr(self, item, value, std_sfp_thr_info):
+        log_info = ""
+        re_rule = r"-?\d+(?:\.\d+)?"
+        data_valid = True
+
+        if bool(re.search(r'\d', value)):
+            value_val = float(re.findall(re_rule, value)[0])
+        else:
+            # print(item,value)
+            log_info += "\tWarning: Invalid %slowwarning data:%s\n" %(item, value)
+            return log_info
+
+        highalarm =  std_sfp_thr_info.get(item + 'highalarm')
+        if bool(re.search(r'\d', highalarm)):
+            highalarm_val =  float(re.findall(re_rule, highalarm)[0])
+            if value_val > highalarm_val:
+                log_info += "\tAlarm  : current %s value: %s  is higher than highalarm: %s\n"\
+                            %(item, value, highalarm)
+        else:
+            log_info += "\tWarning: Invalid %shighalarm data:%s\n" %(item, highalarm)
+
+        lowalarm =  std_sfp_thr_info.get(item + 'lowalarm')
+        if bool(re.search(r'\d', lowalarm)):
+            lowalarm_val =  float(re.findall(re_rule, lowalarm)[0])
+            if value_val < lowalarm_val:
+                log_info += "\tAlarm  : current %s value: %s  is lower than lowalarm: %s\n"\
+                            %(item, value, lowalarm)
+        else:
+            log_info += "\tWarning: Invalid %slowalarm data:%s\n" %(item, lowalarm)
+
+        highwarning =  std_sfp_thr_info.get(item + 'highwarning')
+        if bool(re.search(r'\d', highwarning)):
+            highwarning_val =  float(re.findall(re_rule, highwarning)[0])
+            if value_val > highwarning_val:
+                log_info += "\tWarning: current %s value: %s  is higher than highwarning: %s\n"\
+                            %(item, value, highwarning)
+        else:
+            log_info += "\tWarning: Invalid %shighwarning data:%s\n" %(item, highwarning)
+
+        lowwarning =  std_sfp_thr_info.get(item + 'lowwarning')
+        if bool(re.search(r'\d', lowwarning)):
+            lowwarning_val =  float(re.findall(re_rule, lowwarning)[0])
+            if value_val < lowwarning_val:
+                log_info += "\tWarning: current %s value: %s  is lower than lowwarning: %s\n"\
+                            %(item, value, highwarning)
+        else:
+            log_info += "\tWarning: Invalid %slowwarning data:%s\n" %(item, lowwarning)
+        return log_info
+
+    def check_sfp_vars(self, cur_sfp_var_info, std_sfp_thr_info):
+        log_info = ""
+        cur_sfp_var_info_channel = cur_sfp_var_info['ChannelMonitorValues']
+        std_sfp_thr_info_channel = std_sfp_thr_info
+        # print(cur_sfp_var_info_channel,std_sfp_thr_info_channel)
+        for key,value in cur_sfp_var_info_channel.items():
+            if re.match(r"RX\dPower", key):
+                log_info += self.check_sfp_var_with_thr('rxpower', value, std_sfp_thr_info_channel)
+            if re.match(r"TX\dPower", key):
+                log_info += self.check_sfp_var_with_thr('txpower', value, std_sfp_thr_info_channel)
+            if re.match(r"TX\dBias", key):
+                log_info += self.check_sfp_var_with_thr('txbias', value, std_sfp_thr_info_channel)
+
+        cur_sfp_var_info_module = cur_sfp_var_info['ModuleMonitorValues']
+        std_sfp_thr_info_module = std_sfp_thr_info
+        for key,value in cur_sfp_var_info_module.items():
+            if key == 'Temperature':
+                log_info += self.check_sfp_var_with_thr('temp', value, std_sfp_thr_info_module)
+            if key == 'Vcc':
+                log_info += self.check_sfp_var_with_thr('vcc', value, std_sfp_thr_info_module)
+
+        return log_info
+
+
+    def do_check_sfp(self, port_num):
+        print ("port_num============================================%d",port_num)
+        ELP_flag = 0
+        if self.get_presence(port_num) and self.stand_sfp_table[port_num-1]:
+            if self.stand_sfp_table[port_num-1]['interface']['data']['Connector'] == 'No separable connector':
+                ELP_flag = 1
+
+            cur_sfp_table = self.get_eeprom_dict(port_num)
+            cur_sfp_fix_info = cur_sfp_table["interface"]['data']
+            cur_sfp_fix_threshold_info = self.get_transceiver_dom_threshold_info_dict(port_num)
+            # print(cur_sfp_fix_info)
+            if not ELP_flag:
+                cur_sfp_fix_info.update(cur_sfp_fix_threshold_info)
+            # print(cur_sfp_fix_info)
+            cur_sfp_var_info = cur_sfp_table["dom"]['data']
+            std_sfp_fix_info = self.stand_sfp_table[port_num - 1]["interface"]['data']
+            if not ELP_flag:
+                std_sfp_fix_info.update(self.stand_sfp_table[port_num - 1]["threshold"])
+            std_sfp_thr_info = self.stand_sfp_table[port_num - 1]["threshold"]
+
+            fix_result = self.check_sfp_fixed(cur_sfp_fix_info, std_sfp_fix_info)
+            var_result = self.check_sfp_vars(cur_sfp_var_info, std_sfp_thr_info)
+
+            if len(fix_result) > 0:
+                print('Most recently dumped info is:\n',cur_sfp_fix_info)
+                print('Stored dumped info is:\n',std_sfp_thr_info)
+                return fix_result + var_result + "\tSUMMARY: SFP EEPROM Test Failed!"
+            return var_result + "\tSUMMARY: SFP EEPROM Test Pass!"
+        elif not self.stand_sfp_table[port_num-1]:
+             return "invalide stand_sfp.json file, please check it!"
+
+# --- add for optical module eeprom content validation --- #
